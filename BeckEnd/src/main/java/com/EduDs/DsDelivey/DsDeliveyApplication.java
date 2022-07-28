a8345909e67735417b555999ddc4ab48ea07021f
Eduardo Oliveira Ramos <eduardo.oliveiraramos12@hotmail.com>
2022-07-28 09:07:16 -0300

    initial project

---
+++ BeckEnd/src/main/java/com/EduDs/DsDelivey/DsDeliveyApplication.java
@@ -0,0 +1,13 @@
+package com.EduDs.DsDelivey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsDeliveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsDeliveyApplication.class, args);
+	}
+
+}
---
+++ BeckEnd/src/test/java/com/EduDs/DsDelivey/DsDeliveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EduDs.DsDelivey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsDeliveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
