a48f7df74d2e4485c0a025a34a3625e1fb28938f
Eduardo Oliveira Ramos <eduardo.oliveiraramos12@hotmail.com>
2022-08-23 17:54:08 -0300

    add home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css';
-import Navbar from './NavBar';
-
+import "./App.css";
+import Home from "./Home";
+import Navbar from "./NavBar";
 
 function App() {
-  return <Navbar/>
-
+  return (
+    <>
+      <Navbar />
+      <Home/>
+    </>
+  );
 }
 
 export default App;
