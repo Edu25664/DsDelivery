89cdaff096767517e6b6d5e194627e268371e8e4
Eduardo Oliveira Ramos <eduardo.oliveiraramos12@hotmail.com>
2022-08-23 22:25:36 -0300

    add footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,24 @@
+import './style.css';
+import { ReactComponent as GitHub } from "./iconmonstr-github-1.svg";
+import {ReactComponent as Linkedin} from "./iconmonstr-linkedin-2.svg";
+
+function Footer() {
+  return (
+    <>
+      <footer className="main-footer">
+        <p>App desenvolvido com muito carinho por Eduardo Oliveira Ramos</p>
+        <div className="footer-icons">
+          <a href="https://github.com/Edu25664" target="_new">
+            <GitHub />
+          </a>
+
+          <a href="https://www.linkedin.com/in/ramosedu/" target="_new">
+            <Linkedin/>
+          </a>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
